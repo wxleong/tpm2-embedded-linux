e4d70eb8cde24b77d1dcb7195a056680e70f407c
wenxin.leong <wenxin.leong@infineon.com>
2022-02-28 14:44:57 +0800

    bug fix

--- README.md
+++ README.md
@@ -8,15 +8,19 @@ OPTIGA™ TPM 2.0 on embedded systems.
 
 - Tested on Raspberry Pi 4 Model B with Iridium 9670 TPM 2.0 board [[1]](#1) 
 - Set up the Raspberry Pi according to [[2]](#2) but skipping section "Set Up TSS and Tools"
+- Install dependencies:
+    ```
+    $ sudo apt install cmake
+    ```
 
 # Mbed TLS Library
 
 ```
 $ git clone https://github.com/ARMmbed/mbedtls ~/mbedtls
 $ cd ~/mbedtls
-$ git checkout mbedtls-2.28.0 
-$ cd library
-$ make
+$ git checkout mbedtls-2.28.0
+$ cd library/
+$ make -j$(nproc)
 ```
 
 # Decouple Tpm2-tss Library
@@ -29,8 +33,9 @@ $ git checkout 3.2.0
 $ git clone https://github.com/wxleong/tpm2-embedded ~/tpm2-embedded
 $ cp -r ~/tpm2-embedded/tpm2-tss/cmake ~/tpm2-tss/
 $ cd ~/tpm2-tss/cmake
-$ cmake .
-$ cmake --build .
+$ rm -rf CMakeFiles
+$ cmake -j$(nproc) .
+$ cmake --build . -j$(nproc)
 ```
 
 # Sample Application
@@ -39,7 +44,7 @@ $ cmake --build .
 $ git clone https://github.com/wxleong/tpm2-mbedtls ~/tpm2-mbedtls
 $ cp -f ~/tpm2-embedded/tpm2-mbedtls/Makefile ~/tpm2-mbedtls/code/
 $ cd ~/tpm2-mbedtls/code 
-$ make
+$ make -j$(nproc)
 $ ./main
 ```
 
--- tpm2-tss/cmake/tpm2tss.cmake
+++ tpm2-tss/cmake/tpm2tss.cmake
@@ -1,4 +1,5 @@
 set(TPM2TSS_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../)
+set(MBEDTLS_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/../../mbedtls/)
 
 file(GLOB_RECURSE TSS2_MU_FILES ${TPM2TSS_ROOT_DIR}/src/tss2-mu/*.c)
 file(GLOB_RECURSE TSS2_RC_FILES ${TPM2TSS_ROOT_DIR}/src/tss2-rc/*.c)
@@ -7,15 +8,17 @@ file(GLOB_RECURSE TSS2_ESYS_FILES ${TPM2TSS_ROOT_DIR}/src/tss2-esys/*.c)
 set(TSS2_TCTI_FILES ${TPM2TSS_ROOT_DIR}/src/tss2-tcti/tcti-common.c
                     ${TPM2TSS_ROOT_DIR}/src/tss2-tcti/tctildr.c
                     ${TPM2TSS_ROOT_DIR}/src/tss2-tcti/tctildr-nodl.c
-                    ${TPM2TSS_ROOT_DIR}/src/tss2-tcti/tcti-mssim.c
+#                    ${TPM2TSS_ROOT_DIR}/src/tss2-tcti/tcti-mssim.c
+                    ${TPM2TSS_ROOT_DIR}/src/tss2-tcti/tcti-device.c
 )
-file(GLOB_RECURSE MBEDTLS_FILES /home/wenxinleong/tpm2-mbedtls/code/mbedtls-2.28.0/*.a)
+file(GLOB_RECURSE MBEDTLS_FILES ${MBEDTLS_ROOT_DIR}/library/*.a)
 
 message(STATUS "TSS2_MU_FILES: ${TSS2_MU_FILES}")
 message(STATUS "TSS2_RC_FILES: ${TSS2_RC_FILES}")
 message(STATUS "TSS2_SYS_FILES: ${TSS2_SYS_FILES}")
 message(STATUS "TSS2_ESYS_FILES: ${TSS2_ESYS_FILES}")
 message(STATUS "TSS2_TCTI_FILES: ${TSS2_TCTI_FILES}")
+message(STATUS "MBEDTLS_FILES: ${MBEDTLS_FILES}")
 
 #list(REMOVE_ITEM TSS2_ESYS_FILES ${TPM2TSS_ROOT_DIR}/src/tss2-esys/esys_crypto_mbed.c)
 list(REMOVE_ITEM TSS2_ESYS_FILES ${TPM2TSS_ROOT_DIR}/src/tss2-esys/esys_crypto_ossl.c)
@@ -31,17 +34,18 @@ add_library(tpm2tss STATIC
   ${TPM2TSS_ROOT_DIR}/src/util/key-value-parse.c
 )
 
-target_link_libraries(tpm2tss PRIVATE iface_obj3)
+target_link_libraries(tpm2tss PRIVATE ${MBEDTLS_FILES})
 
 target_include_directories(tpm2tss
   PRIVATE
   ${TPM2TSS_ROOT_DIR}/src
   ${TPM2TSS_ROOT_DIR}/src/tss2-sys
   ${TPM2TSS_ROOT_DIR}/src/tss2-esys
-  /home/wenxinleong/tpm2-mbedtls/code/mbedtls-2.28.0/include
+  ${MBEDTLS_ROOT_DIR}/include
   PUBLIC
   ${TPM2TSS_ROOT_DIR}/include/tss2
 )
 
-#target_compile_definitions(tpm2tss PUBLIC -DMAXLOGLEVEL=6 -DTCTI_MSSIM -DOSSL=1)
-target_compile_definitions(tpm2tss PUBLIC -DMAXLOGLEVEL=6 -DTCTI_MSSIM -DMBED=1)
+#target_compile_definitions(tpm2tss PUBLIC -DMAXLOGLEVEL=6 -DTCTI_DEVICE -DOSSL=1)
+target_compile_definitions(tpm2tss PUBLIC -DMAXLOGLEVEL=6 -DTCTI_DEVICE -DMBED=1)
+#target_compile_definitions(tpm2tss PUBLIC -DMAXLOGLEVEL=6 -DTCTI_MSSIM -DMBED=1)
